71ef5e51bc3fab711ddd7bf0d420a60857d57dbc
Milosz Dura <milosz.md@wp.pl>
2015-10-31 12:15:45 +0000

    composer added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	require 'vendor/autoload.php';
+
+	use Techblog\User;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Techblog</title>
+</head>
+<body>
+	<h1>Techblog</h1>
+	<p><?php echo User::test(); ?></p>
+</body>
+</html>
---
+++ src/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace Techblog;
+
+class User
+{
+	public static function test(){
+		return 'Hello World!';
+	}
+}
