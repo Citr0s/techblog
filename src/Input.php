928d88ac6e69f009961faeb71c5e31c971beda74
Milosz Dura <milosz.md@wp.pl>
2015-11-04 20:55:35 +0000

    documentation added

--- src/Input.php
+++ src/Input.php
@@ -6,36 +6,59 @@ use Exception;
 class Input
 {
 	/*
+		Checks if email is valid.
+		@param $email
+	*/
+	private static function isValidEmail($email){
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			throw new Exception("Valid email address required.");
+		}
+	}
+
+	/*
+		Checks if actual match expected inputs.
+		@param $email
+	*/
+	private static function hasEnoughInputs($input, $expected){
+		if(count($input) !== count($expected)){
+			throw new Exception("All inputs are required.");
+		}
+	}
+
+	/*
+		Checks if any fields are empty;
+		@param $email
+	*/
+	private static function hasEmptyInputs($input){
+		foreach($input as $field){
+			if(empty($field)){
+				throw new Exception("All fields are requierd.");
+			}
+		}
+	}
+	/*
+		Sanitises input.
 		@param <array> $input
 		@return <bool> $result
 	*/
 	public static function sanitise($input){
 		foreach($input as $field => $value){
 			if($field === 'email'){
-				if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
-					throw new Exception("Valid email address required.");
-				}
+				self::isValidEmail($value);
 			}
 			htmlentities($field);
 		}	
 	}
 
 	/*
+		Checks for correct input.
 		@param <array> $input
 		@param <array> $expected
 		@return <bool> $result
 	*/
 	public static function check($input, $expected){
-		if(count($input) !== count($expected)){
-			throw new Exception("All inputs are required.");
-		}
-
-		foreach($input as $field){
-			if(empty($field)){
-				throw new Exception("All fields are requierd.");
-			}
-		}
-
+		self::hasEnoughInputs($input, $expected);
+		self::hasEmptyInputs($input);
 		self::sanitise($input);
 
 		return true;
