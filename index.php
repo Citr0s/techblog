50beae63a823fb7d981fe3a224824c88f4f6dfb5
Milosz Dura <milosz.md@wp.pl>
2015-11-04 10:29:11 +0000

    split html into include files

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
 
 	use Techblog\User;
 	use Techblog\Database;
+
+	include 'inc/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Techblog</title>
-</head>
-<body>
-	<h1>Techblog</h1>
-	<p><?php echo User::test(); ?></p>
-</body>
-</html>
+<h1>Techblog</h1>
+<p><?php echo User::test(); ?></p>
+<?php
+	include 'inc/footer.php';
+?>
